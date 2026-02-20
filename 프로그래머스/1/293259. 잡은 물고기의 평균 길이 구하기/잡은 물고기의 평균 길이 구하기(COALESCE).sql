62b42290f272b54c751b93e2d292f1996851d3aa
이혜림 <153624836+leesunforest@users.noreply.github.com>
2026-02-20 14:02:24 +0900

    Create 잡은 물고기의 평균 길이 구하기(COALESCE).sql

---
+++ 프로그래머스/1/293259. 잡은 물고기의 평균 길이 구하기/잡은 물고기의 평균 길이 구하기(COALESCE).sql
@@ -0,0 +1,7 @@
+-- 문제 : 잡은 물고기의 평균 길이를 출력
+-- 컬럼 : AVERAGE_LENGTH
+-- 테이블 : FISH_INFO 
+-- 조건 : 10cm 이하의 물고기들은 10cm 로 취급
+SELECT
+    ROUND(AVG(COALESCE(LENGTH, 10)), 2) AS AVERAGE_LENGTH
+FROM FISH_INFO
