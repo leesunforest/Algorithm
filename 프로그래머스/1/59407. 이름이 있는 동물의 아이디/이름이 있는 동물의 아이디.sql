7b350c31bfcf14d8389c54101cef394b9d6a14df
이혜림 <153624836+leesunforest@users.noreply.github.com>
2026-02-20 13:36:38 +0900

    [level 1] Title: 이름이 있는 동물의 아이디, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/59407. 이름이 있는 동물의 아이디/이름이 있는 동물의 아이디.sql
@@ -0,0 +1,10 @@
+-- 문제 : 동물 보호소에 들어온 동물 중, 이름이 있는 동물의 ID를 조회
+-- 컬럼 : ANIMAL_ID
+-- 테이블 : ANIMAL_INS 
+-- 조건 : 이름이 있는
+-- 정렬 : ID 오름차순
+SELECT
+    ANIMAL_ID
+FROM ANIMAL_INS 
+WHERE NAME IS NOT NULL
+ORDER BY ANIMAL_ID
